25e033d2db5ea24c4f64ad3e57a6b6194bdaab0d
beepscore <support@beepscore.com>
2010-03-18 16:47:51 -0700

    Add DrawView class, change DrawViewController.xib view class to DrawView. Ref Dudney p377

---
+++ Classes/DrawView.h
@@ -0,0 +1,16 @@
+//
+//  DrawView.h
+//  Draw
+//
+//  Created by Steve Baker on 3/18/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DrawView : UIView {
+
+}
+
+@end
---
+++ Classes/DrawView.m
@@ -0,0 +1,33 @@
+//
+//  DrawView.m
+//  Draw
+//
+//  Created by Steve Baker on 3/18/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import "DrawView.h"
+
+
+@implementation DrawView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
